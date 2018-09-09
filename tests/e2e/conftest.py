7303d6bcdbbd90a8499d6a18f593378927b6dd2a
thomaswtsang <thomaswtsang@gmail.com>
2018-09-09 16:08:45 -0400

    Set up fixture for page object

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,6 +1,15 @@
 import pytest
 from selenium import webdriver
+from tests.e2e.page_objects import LoginPage
+
+
+@pytest.fixture
+def driver():
+    return webdriver.Firefox()
 
 @pytest.fixture()
-def driver():
-    return webdriver.Chrome()
+def LoginPageObject(driver):
+    # TODO abstract URI
+    page = LoginPage(driver, root_uri="https://splashthat.com")
+    page.get("/login")
+    return page
