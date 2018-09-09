30eb3d0548f8aa3d7a2fd8256f34c2744f45308c
thomaswtsang <thomaswtsang@gmail.com>
2018-09-09 16:09:05 -0400

    Set up login page object

--- tests/e2e/page_objects.py
+++ tests/e2e/page_objects.py
@@ -1,12 +1,19 @@
 from page_objects import PageObject, PageElement
 
 class LoginPage(PageObject):
-    username = PageElement(name = "data[User][email]")
+    # username needs a more unique identifier
+    username = PageElement(class_name = "text-input")
     password = PageElement(id_ = "password2")
     submit = PageElement(class_name = "green-button")
     
     def fill_in_form(self):
-        pass
+        # TODO abstract user input
+        self.username = "qacandidate@splashthat.com"
+        self.password = "testingislife"
 
     def login(self):
-        pass
+        self.fill_in_form()
+        self.submit.click()
+
+    def is_logged_in(self):
+        return "events" in self.w.current_url
