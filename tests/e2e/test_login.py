0d80205070187229d56cdf7b87722ca8c1e19b5a
thomaswtsang <thomaswtsang@gmail.com>
2018-09-09 16:09:18 -0400

    Add test for loginpage

--- tests/e2e/test_login.py
+++ tests/e2e/test_login.py
@@ -1,8 +1,9 @@
 
 class TestLoginPage(object):
     
-    def test_login_success(self):
-        pass
+    def test_login_success(self, LoginPageObject):
+        LoginPageObject.login()
+        assert LoginPageObject.is_logged_in()
 
     def test_login_failure(self):
         pass
