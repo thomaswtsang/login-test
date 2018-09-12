571c2a31d3bc350bc6bc5ae700a91e87ae27c0b6
thomaswtsang <thomaswtsang@gmail.com>
2018-09-11 23:45:47 -0400

    Test remember login

--- tests/e2e/test_login.py
+++ tests/e2e/test_login.py
@@ -9,3 +9,9 @@ class TestLoginPage(object):
     def test_login_failure(self, LoginPageObject):
         LoginPageObject.login(**bad_user)
         assert not LoginPageObject.is_logged_in()
+
+    def test_remember_me(self, LoginPageObject):
+        LoginPageObject.login(remember=True, **test_user)
+        LoginPageObject.load()
+        assert LoginPageObject.is_logged_in()
+
